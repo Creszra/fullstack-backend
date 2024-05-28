f0791cd6d3788027e426c83a73744ed8bfe416e8
Creszra <josephrmagabilin@gmail.com>
2024-05-29 05:37:24 +0800

    da

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const usersRouter = require("./routes/Users");
 app.use("/auth", usersRouter);
 
 db.sequelize.sync().then(() => {
-  app.listen(3001, () => {
+  app.listen(process.env.port || 3001, () => {
     console.log("Server running on port 3001");
   });
 });
